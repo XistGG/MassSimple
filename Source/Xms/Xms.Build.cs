674bf18bd75a09b0aec6148c45af7330a2b5d4c4
Xist <xist@xist.gg>
2025-11-08 13:04:32 -0800

    Add WITH_XMS_DEBUG in DebugGame configuration

--- Source/Xms/Xms.Build.cs
+++ Source/Xms/Xms.Build.cs
@@ -6,6 +6,10 @@ public class Xms : ModuleRules
 {
 	public Xms(ReadOnlyTargetRules Target) : base(Target)
 	{
+		// Add XMS DEBUG code in DebugGame
+		int WithXmsDebug = Target.Configuration == UnrealTargetConfiguration.DebugGame ? 1 : 0;
+		PublicDefinitions.Add("WITH_XMS_DEBUG="+WithXmsDebug);
+
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicIncludePaths.AddRange(new string[] { "Xms" });
